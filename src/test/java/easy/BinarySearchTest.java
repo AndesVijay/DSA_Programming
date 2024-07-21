d8745303157a8caa117d46cebd8f20d74c248ea7
AndesVijay <hackerboyandesvijay7890@gmail.com>
2024-07-21 12:40:18 +0530

    binarySearch Implemnted

---
+++ src/test/java/easy/BinarySearchTest.java
@@ -0,0 +1,16 @@
+package easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+    @org.junit.jupiter.api.Test
+    void implementBinarySearch() {
+        BinarySearch b1= new BinarySearch();
+        assertTrue(b1.implementBinarySearch(new int[]{1,2,3,4,5,6},5));
+        assertFalse(b1.implementBinarySearch(new int[]{1,2,3,4,5,6},50));
+        assertFalse(b1.implementBinarySearch(new int[]{1,2,3,4,5,6},50));
+        assertTrue(b1.implementBinarySearch(new int[]{}, 50));
+        assertFalse(b1.implementBinarySearch(new int[]{}, 50));
+    }
+}
