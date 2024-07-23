00ed2c9f6c2d88de2c07d90acf94955c651b7b87
AndesVijay <hackerboyandesvijay7890@gmail.com>
2024-07-23 12:16:58 +0530

    check palindrome

---
+++ src/main/java/org/example/two_pointers/TwoPointerProblem.java
@@ -0,0 +1,17 @@
+package org.example.two_pointers;
+
+public class TwoPointerProblem {
+    public boolean isPalindrome(String str )
+    {
+        int left = 0,right = str.length()-1;
+        if (str.isEmpty())
+            return false;
+        while(left<=right){
+
+            if(str.charAt(left)!=str.charAt(right))
+                return false;
+            left++;right--;
+    }
+        return true;
+    }
+}
